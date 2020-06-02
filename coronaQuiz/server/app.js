fe90d5348c4f4f0df23f31fa997d46f1bb601f12
rahul-chawla-vst-au4 <54412646+rahul-chawla-vst-au4@users.noreply.github.com>
2020-06-02 18:34:37 +0530

    ""

--- coronaQuiz/server/app.js
+++ coronaQuiz/server/app.js
@@ -16,7 +16,7 @@ app.use(express.static("client/build"));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV.trim() === "production") {
 } 
 var indexRouter = require("./routes/index");
 app.use("/", indexRouter);
