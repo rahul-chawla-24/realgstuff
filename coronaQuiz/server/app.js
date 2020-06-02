612026846f0583e43fa88e7ec03234ecf3ba2b39
rahul-chawla-vst-au4 <54412646+rahul-chawla-vst-au4@users.noreply.github.com>
2020-06-02 19:08:00 +0530

    ""

--- coronaQuiz/server/app.js
+++ coronaQuiz/server/app.js
@@ -11,13 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static("client/build"));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-if (process.env.NODE_ENV.trim() === "production") {
-} 
 var indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
--- coronaQuiz/server/routes/index.js
+++ coronaQuiz/server/routes/index.js
@@ -1,15 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 /* GET home page. */
-var questionRouter = require('./questions');
-var answerRouter = require('./answers');
-var userRouter = require('./users');
- 
+var questionRouter = require("./questions");
+var answerRouter = require("./answers");
+var userRouter = require("./users");
 
-router.use('/user', userRouter);
-router.use('/answers', answerRouter);
-router.use('/questions', questionRouter);
+if (process.env.NODE_ENV === "production") {
+  router.use(express.static(path.join(__dirname, "/client/build")));
+  router.get("*", function (req, res) {
+    res.sendFile("index.html", {
+      root: path.join(__dirname, "../client/build"),
+    });
+  });
+}
 
+router.use("/user", userRouter);
+router.use("/answers", answerRouter);
+router.use("/questions", questionRouter);
 
 module.exports = router;
